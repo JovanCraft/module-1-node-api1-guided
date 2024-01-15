6c144febf901885dd888e746fbe772b4ff0bc996
jovan <jovancraft@gmail.com>
2024-01-15 14:13:40 -0600

    used postman to check out my hello world endpoint

--- api/server.js
+++ api/server.js
@@ -5,10 +5,10 @@ const server = express()
 // GLOBAL MIDDLEWARE
 server.use(express.json())
 // ENDPOINTS
+// [GET]    /             (Hello World endpoint)
 server.get('/hello-world', (req, res) => {
     res.status(200).json({ message: "hello, world!" })
 })
-// [GET]    /             (Hello World endpoint)
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
